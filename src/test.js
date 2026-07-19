0d8acc55bc4007fc533fcdeec46eef006f7fcf84
zypher-zmj <zmj15838564734@gmail.com>
2026-07-19 20:36:28 +0800

    脱敏

--- src/test.js
+++ src/test.js
@@ -15,3 +15,21 @@ Promise.newAllSettled = (arr) => {
     });
   });
 };
+
+Promise.aAllSettled = (arr) => {
+  let result = [];
+  return new Promise((res, rej) => {
+    arr.forEach((val) => {
+      Promise.resolve(val)
+        .then((data) => {
+          result.push({ status: "fullFilled", val: data });
+        })
+        .catch((err) => {
+          result.push({ status: "rejected", val: err });
+        });
+      if (result.length === arr.length) {
+        res(result);
+      }
+    });
+  });
+};
