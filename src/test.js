cd4083f91a800962b66defc508119b5c651a093a
zypher-zmj <zmj15838564734@gmail.com>
2026-07-19 17:46:55 +0800

    feat:新增test

---
+++ src/test.js
@@ -0,0 +1,17 @@
+Promise.newAllSettled = (arr) => {
+  let result = [];
+  return new Promise((res, rej) => {
+    arr.forEach((val) => {
+      Promise.resolve(val)
+        .then((data) => {
+          result.push({ status: "fullFilled", val: data });
+        })
+        .catch((err) => {
+          result.push({ status: "rejected", val: err });
+        });
+      if (result.length === arr.length) {
+        res(result);
+      }
+    });
+  });
+};
